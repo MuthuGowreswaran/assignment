286c6345a0981dae6be2da43e84c4cb0fe1e2e0d
MuthuGowreswaran <128568399+MuthuGowreswaran@users.noreply.github.com>
2023-03-24 18:19:49 +0530

    Add files via upload

---
+++ PageObjectModel/ViewLead.java
@@ -0,0 +1,24 @@
+package PageObjectModel;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import Base.BaseClass;
+
+public class ViewLead extends BaseClass {
+	
+	public ViewLead(ChromeDriver driver) {
+		this. driver= driver;
+		
+	}
+	
+	public ViewLead verify() {
+		
+		
+		System.out.println("The Create Lead Page is Verifyed");
+		
+		return this;
+	}
+	
+	
+
+}
